60e44c4e0105adbaf47bbdcf7c92924a04bebfc3
filomenoPonce580 <110742301+filomenoPonce580@users.noreply.github.com>
2023-02-21 10:32:42 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function descendingOrder(n){
-  //split, sort high to low, join, turn into number and return
-  return Number(n.split('').sort((a,b)=>b-a).join(''))
+  //turn to string, split, sort high to low, join, turn into number and return
+  return Number(n.toString().split('').sort((a,b)=>b-a).join(''))
 }
